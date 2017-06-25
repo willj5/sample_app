20ad8044d70490fa965320dbbdbc81111766888d
Jason Williams <terrd2713@gmail.com>
2017-06-25 01:05:14 -0500

    commented out win32console

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'win32console'
+# require 'win32console'
 require "minitest/reporters"
 Minitest::Reporters.use!
 
